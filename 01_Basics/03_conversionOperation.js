0874a7b81fd5548b94891284cd2502ce50cfd81c
SaurabhTiwari95 <41651293+SaurabhTiwari95@users.noreply.github.com>
2023-09-04 12:27:54 +0000

    conversion lecture version 1

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = 33
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+let score = "saurabh"
+ 
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+/* 
+    The Above Code will return *number* while the below line where *Number*
+    is written depicts that it is a class base conversion
+*/ 
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn)
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn)
