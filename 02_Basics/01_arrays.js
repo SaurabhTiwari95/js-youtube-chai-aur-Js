eee5ba2abac0171bf3528e426e80a4d809ed807b
SaurabhTiwari95 <41651293+SaurabhTiwari95@users.noreply.github.com>
2023-09-24 12:43:11 +0000

    Arrays part 1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,14 @@
+// Array : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+const myArr = [ 0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman","nagraaj"]
+
+/*
+    Arrays create a shallow copy everytime which means the original array will also be changed.
+    Shallow Copy of an object is a copy whose properties share the same reference point.
+
+    Deep Copy of an object is a copy whose properties do not share the same reference point.
+*/ 
+
+/*---------------------------------------Array Methods---------------------------------------*/ 
+
+const myArr2 = new Array(1,2,3,4)
