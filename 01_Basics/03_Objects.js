b0bb299bfc8022ab1c2135789542f69a361088a9
SaurabhTiwari95 <41651293+SaurabhTiwari95@users.noreply.github.com>
2023-11-05 05:44:32 +0000

    objects Part 1

---
+++ 01_Basics/03_Objects.js
@@ -0,0 +1,19 @@
+// singleton
+// Object.create
+
+// object literals
+
+/*------------------We are talking about the object literals here------------------*/ 
+const JsUser = {
+    name : "Saurabh",
+    age : "28",
+    location : "gurgaon",
+    email : "saurabh@google.com",
+    isLoggedIn : false,
+    lastLoggedIn : ["Monday", "Saturday"]
+}
+
+/*----How to access the object values-----*/ 
+
+console.log(JsUser.email)
+// this is wrong way to access the objects
